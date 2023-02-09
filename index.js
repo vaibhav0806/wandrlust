3da4c202d4a7a135f543a773d894db3bd44d744b
Abhishek Bhagat <abhishekb740@gmail.com>
2023-02-09 16:32:08 +0530

    Mongodb database Connection established.

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const path = require('path');
+const {DB_URL} = require("./config");
+const {mongoose} = require("mongoose");
+
 const app = express();
 
 var PORT = process.env.PORT || 6969
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'))
+
+const connect = () =>{
+    try{
+        mongoose.set('strictQuery', true);
+        mongoose.connect(DB_URL);
+        console.log("Connected to MongoDb");
+    }catch(err){
+        console.log(err);
+    }
+}
 
 app.get('/', (req, res) => {
     res.render('home');
@@ -18,5 +31,7 @@ app.listen(PORT, err => {
     if(err) {
         console.log(err);
     }
+    connect();
+    console.log(DB_URL);
     console.log('Listening on port ', PORT);
 })
