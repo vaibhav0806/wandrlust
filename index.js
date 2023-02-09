acda822b4b555fac2ed36983641c36c09d0aadb1
Vaibhav Pandey <vaibhav.pandey0806@gmail.com>
2023-02-09 15:31:16 +0530

    Added view folders and homepage

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
 
 var PORT = process.env.PORT || 6969
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'))
 
 app.get('/', (req, res) => {
     res.send("HELLO");
+});
+
+app.get('/home', (req, res) => {
+    res.render('home');
 });
 
 app.listen(PORT, err => {
