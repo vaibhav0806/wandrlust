bd4c8a73c3aa26b0a619b5d332e78bfa3731eaed
Vaibhav Pandey <vaibhav.pandey0806@gmail.com>
2023-02-16 15:59:11 +0530

    Modified index.js

--- index.js
+++ index.js
@@ -24,6 +24,10 @@ app.get('/', (req, res) => {
     res.render('home');
 });
 
+app.get('/about', (req, res) => {
+    res.render('about')
+});
+
 app.get('/test', (req, res) => {
     res.render('test');
 });
