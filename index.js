ebd7e315d55ca3eef08ebb781a2bae645e186d95
abhishekb740 <abhishekb740@gmail.com>
2023-03-28 00:57:27 +0530

    Other minor changes in gitignore, index.js and package.json

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const path = require("path");
 const { DB_URL } = require("./config");
 const User = require("./Models/user");
 const { mongoose } = require("mongoose");
-var bodyParser = require('body-parser')
+var bodyParser = require("body-parser");
 const AuthRoute = require("./routes/auth");
-const cookieParser = require("cookie-parser")
+const cookieParser = require("cookie-parser");
 
 const app = express();
 
@@ -14,9 +14,8 @@ var PORT = process.env.PORT || 6969;
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 app.use(express.json());
-app.use(bodyParser.json());
-app.use(cookieParser())
-
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 const connect = () => {
   try {
     mongoose.set("strictQuery", true);
@@ -27,56 +26,11 @@ const connect = () => {
   }
 };
 
-// Testing Function
-// const createUser = async (req,res,next) =>{
-//     console.log("Working?");
-//     try{
-//         console.log("Inside");
-//         const testUser = await new User({
-//             name: "TestName1",
-//             phone: +9184733457,
-//             email: "test12@gmail1.com",
-//             username: "flamekaise1r",
-//             password: "test12356",
-//             age: 19,
-//         }).save()
-
-//         console.log(testUser);
-//     }
-//     catch(err){
-//         console.log(err);
-//     }
-// }
-
 mongoose.connection.on("disconnected", () => {
   console.log("Mongodb Disconnected.");
 });
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
-
-app.get("/about", (req, res) => {
-  res.render("about");
-});
-
-app.get("/test", (req, res) => {
-  res.render("test");
-});
-
-app.get("/login", (req, res) => {
-  res.render("login");
-});
-
-app.get("/signup", (req, res) => {
-  res.render("signup");
-});
-
-app.get("*", function (req, res) {
-  res.render("error");
-});
-
-app.use("/",AuthRoute);
+app.use("/", AuthRoute);
 
 app.listen(PORT, (err) => {
   if (err) {
@@ -84,6 +38,4 @@ app.listen(PORT, (err) => {
   }
   connect();
   console.log("Listening on port ", PORT);
-  // createUser();
-  // console.log("User Entry Added");
 });
