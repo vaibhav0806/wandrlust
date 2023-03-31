d0fce0088b9e5e6b88eba920ea4406536539b6d6
abhishekb740 <abhishekb740@gmail.com>
2023-03-31 14:05:33 +0530

    Updated auth.js

--- Controllers/auth.js
+++ Controllers/auth.js
@@ -64,6 +64,7 @@ const login = async (req, res, next) => {
     session.username = user.username;
     session.age = user.age;
     session.gender = user.gender;
+    session._id = user._id;
     res.redirect("/");
   } catch (err) {
     next(err);
