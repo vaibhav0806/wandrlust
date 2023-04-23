2485771b0ba8db9a0bbf0e501060833d41dce98e
abhishekb740 <abhishekb740@gmail.com>
2023-04-23 11:11:53 +0530

    Updated Feeds page and other minor changes

--- routes/auth.js
+++ routes/auth.js
@@ -190,10 +190,6 @@ router.get("/post", (req, res) => {
 });
 
 router.get("/profile", (req, res) => {
-  console.log("Followings ", session.following);
-  console.log("Followers", session.followers);
-  console.log("Following Length", session.following.length);
-  console.log("Followers Length", session.followers.length);
   ImageModel.find({ author: session._id }, (err, images) => {
     if (err) {
       console.log(err);
