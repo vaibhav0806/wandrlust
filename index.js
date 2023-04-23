26ebff73916c471b435819623536bb19c9889a2a
Vaibhav Pandey <vaibhav.pandey0806@gmail.com>
2023-04-23 18:43:23 +0530

    Add code for socket connection

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const express = require("express");
 const path = require("path");
+const http = require('http');
 const { DB_URL } = require("./config");
 const { mongoose } = require("mongoose");
 var bodyParser = require("body-parser");
 const AuthRoute = require("./routes/auth");
 const AdminRoute = require("./routes/admin");
 const cookieParser = require("cookie-parser");
+const socketio = require("socket.io");
+const formatMessage = require("./utils/messages");
+const {
+  userJoin,
+  getCurrentUser,
+  userLeave,
+  getRoomUsers,
+} = require("./utils/users");
+
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 app.use(express.static(path.join(__dirname, "public")));
 var PORT = process.env.PORT || 6969;
@@ -15,6 +27,60 @@ app.set("views", path.join(__dirname, "/views"));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+const botName = "ChatCord Bot";
+
+// Run when client connects
+io.on("connection", (socket) => {
+  socket.on("joinRoom", ({ username, room }) => {
+    const user = userJoin(socket.id, username, room);
+
+    socket.join(user.room);
+
+    // Welcome current user
+    socket.emit("message", formatMessage(botName, "Welcome to ChatCord!"));
+
+    // Broadcast when a user connects
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        "message",
+        formatMessage(botName, `${user.username} has joined the chat`)
+      );
+
+    // Send users and room info
+    io.to(user.room).emit("roomUsers", {
+      room: user.room,
+      users: getRoomUsers(user.room),
+    });
+  });
+
+  // Listen for chatMessage
+  socket.on("chatMessage", (msg) => {
+    const user = getCurrentUser(socket.id);
+
+    io.to(user.room).emit("message", formatMessage(user.username, msg));
+  });
+
+  // Runs when client disconnects
+  socket.on("disconnect", () => {
+    const user = userLeave(socket.id);
+
+    if (user) {
+      io.to(user.room).emit(
+        "message",
+        formatMessage(botName, `${user.username} has left the chat`)
+      );
+
+      // Send users and room info
+      io.to(user.room).emit("roomUsers", {
+        room: user.room,
+        users: getRoomUsers(user.room),
+      });
+    }
+  });
+});
+
 const connect = () => {
   try {
     mongoose.set("strictQuery", true);
@@ -32,7 +98,7 @@ mongoose.connection.on("disconnected", () => {
 app.use("/admin", AdminRoute);
 app.use("/", AuthRoute);
 
-app.listen(PORT, (err) => {
+server.listen(PORT, (err) => {
   if (err) {
     console.log(err);
   }
